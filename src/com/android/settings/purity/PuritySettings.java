13aece3e669be9a594fd5573cc59c11c25f8dfa5
AndroidRul3z <ferrettidario@gmail.com>
2017-04-27 15:11:40 +0200

    Add OMS preference category for OMS application

--- src/com/android/settings/purity/PuritySettings.java
+++ src/com/android/settings/purity/PuritySettings.java
@@ -20,8 +20,13 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceScreen;
+import android.net.Uri;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceGroup;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceCategory;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
@@ -32,6 +37,10 @@ import java.util.List;
 
 public class PuritySettings extends SettingsPreferenceFragment {
 
+    private static final String KEY_OMS_APP = "oms_app";
+
+    private Preference mOmsApp;
+
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.APPLICATION;
@@ -40,7 +49,28 @@ public class PuritySettings extends SettingsPreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.purity_settings);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        mOmsApp = (Preference) findPreference(KEY_OMS_APP);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mOmsApp) {
+            Intent omsIntent = getPackageManager().getLaunchIntentForPackage("projekt.substratum");
+            if (omsIntent != null) {
+                startActivity(omsIntent);
+            } else {
+                downloadOmsApp();
+            }
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    private void downloadOmsApp(){
+        Intent omsDownloadIntent = new Intent(Intent.ACTION_VIEW);
+        omsDownloadIntent.setData(Uri.parse("https://play.google.com/store/apps/details?id=projekt.substratum"));
+        startActivity(omsDownloadIntent);
     }
 }
